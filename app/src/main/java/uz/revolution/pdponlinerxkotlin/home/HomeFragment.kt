f1ccf711c32b79e960abc2591078c7673398ce11
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-03-25 22:44:42 +0500

    Home Fragment

--- app/src/main/java/uz/revolution/pdponlinerxkotlin/home/HomeFragment.kt
+++ app/src/main/java/uz/revolution/pdponlinerxkotlin/home/HomeFragment.kt
@@ -1,14 +1,12 @@
 package uz.revolution.pdponlinerxkotlin.home
 
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -23,7 +21,6 @@ import uz.revolution.pdponlinerxkotlin.entities.Module
 import uz.revolution.pdponlinerxkotlin.home.adapters.GeneralAdapter
 import uz.revolution.pdponlinerxkotlin.home.adapters.ItemHomeAdapter
 import uz.revolution.pdponlinerxkotlin.models.GeneralData
-import java.util.function.Consumer
 
 
 private const val ARG_PARAM1 = "param1"
@@ -40,18 +37,18 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        getDao = AppDatabase.get.getDatabase().getDao()
+
     }
+
+    private val TAG = "AAAA"
 
     lateinit var binding: FragmentHomeBinding
     private var data: ArrayList<GeneralData>? = null
 
-    private var courseList: ArrayList<Course>? = null
-    private var moduleList: ArrayList<Module>? = null
     private var generalAdapter: GeneralAdapter? = null
     private var itemHomeAdapter: ItemHomeAdapter? = null
 
-    private var getDao: AllDaos? = null
+    lateinit var getDao: AllDaos
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -59,6 +56,7 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
+        getDao = AppDatabase.get.getDatabase().getDao()
         loadData()
         innerItemClick()
         allModulesClick()
@@ -89,7 +87,7 @@ class HomeFragment : Fragment() {
         // ItemHomeAdapterni itemi bosilganda ishlaydigan listener
         itemHomeAdapter?.onModuleClick = object : ItemHomeAdapter.OnModuleClick {
             override fun onClick(module: Module) {
-                Toast.makeText(binding.root.context, "Item Clicked", Toast.LENGTH_SHORT).show()
+                Toast.makeText(binding.root.context, "Item Clicked: ${module.moduleName}", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -97,58 +95,63 @@ class HomeFragment : Fragment() {
     @SuppressLint("CheckResult")
     private fun loadData() {
         data = ArrayList()
-        courseList = ArrayList()
-        moduleList = ArrayList()
+        generalAdapter = GeneralAdapter()
+        itemHomeAdapter = ItemHomeAdapter(binding.root.context)
         var courseID: Int
 
-        getDao!!.getAllCourse()
+        getDao.getAllCourse()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(@SuppressLint("NewApi")
-            object : Consumer<List<Course>>, io.reactivex.functions.Consumer<List<Course>> {
-                override fun accept(t: List<Course>) {
-                    courseList = t as ArrayList
+            .subscribe(object : io.reactivex.functions.Consumer<List<Course>> {
+                override fun accept(t: List<Course>?) {
+
+
+                    for (i in 0 until t!!.size) {
+                        courseID = t[i].id!!
+
+
+                        getDao.getModulesByCourceID(courseID)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                                object : io.reactivex.functions.Consumer<List<Module>> {
+                                    override fun accept(p: List<Module>) {
+
+                                        data?.add(GeneralData(t[i], p))
+
+
+
+                                        generalAdapter?.setAdapter(binding.root.context, itemHomeAdapter!!)
+                                        generalAdapter?.submitList(data)
+
+                                    }
+                                },
+                                object : io.reactivex.functions.Consumer<Throwable> {
+                                    override fun accept(p0: Throwable) {
+
+                                    }
+                                }, object : Action {
+                                    override fun run() {
+
+                                    }
+                                })
+                    }
                 }
-            }, @RequiresApi(Build.VERSION_CODES.N)
-            object : Consumer<Throwable>, io.reactivex.functions.Consumer<Throwable> {
-                override fun accept(p0: Throwable) {
+
+            }, object : io.reactivex.functions.Consumer<Throwable> {
+                override fun accept(t: Throwable?) {
 
                 }
+
             }, object : Action {
+
                 override fun run() {
 
                 }
+
             })
 
-        for (i in 0 until courseList!!.size) {
-            courseID = courseList!![i].id!!
-
-            getDao!!.getModulesByCourceID(courseID)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(@RequiresApi(Build.VERSION_CODES.N)
-                object : Consumer<List<Module>>, io.reactivex.functions.Consumer<List<Module>> {
-                    override fun accept(t: List<Module>) {
-                        moduleList = t as ArrayList
-
-                        data?.add(GeneralData(courseList!![i], t))
-                    }
-                }, @RequiresApi(Build.VERSION_CODES.N)
-                object : Consumer<Throwable>, io.reactivex.functions.Consumer<Throwable> {
-                    override fun accept(p0: Throwable) {
-
-                    }
-                }, object : Action {
-                    override fun run() {
-
-                    }
-                })
-        }
-
-        itemHomeAdapter = ItemHomeAdapter(binding.root.context)
-        generalAdapter = GeneralAdapter(binding.root.context, data as ArrayList, itemHomeAdapter!!)
         binding.homeMainRv.adapter = generalAdapter
-
 
     }
 }
--- app/src/main/java/uz/revolution/pdponlinerxkotlin/home/adapters/GeneralAdapter.kt
+++ app/src/main/java/uz/revolution/pdponlinerxkotlin/home/adapters/GeneralAdapter.kt
@@ -4,21 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import uz.revolution.pdponlinerxkotlin.R
 import uz.revolution.pdponlinerxkotlin.databinding.ItemGeneralHomeBinding
 import uz.revolution.pdponlinerxkotlin.entities.Course
-import uz.revolution.pdponlinerxkotlin.entities.Module
 import uz.revolution.pdponlinerxkotlin.models.GeneralData
 
-class GeneralAdapter(
-    var context: Context,
-    var data: ArrayList<GeneralData>,
-    var itemHomeAdapter: ItemHomeAdapter
-) :
-    RecyclerView.Adapter<GeneralAdapter.VH>() {
+class GeneralAdapter : ListAdapter<GeneralData, GeneralAdapter.VH>(MyDiffUtil()) {
 
     var onAllItemClick: OnAllItemClick? = null
+    var context: Context? = null
+    var itemHomeAdapter: ItemHomeAdapter? = null
+
+    fun setAdapter(context: Context, itemHomeAdapter: ItemHomeAdapter) {
+        this.context = context
+        this.itemHomeAdapter = itemHomeAdapter
+    }
 
     inner class VH(var binding: ItemGeneralHomeBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -28,7 +31,7 @@ class GeneralAdapter(
             binding.root.animation = AnimationUtils.loadAnimation(context, R.anim.anim1)
 
 //            val itemHomeAdapter = ItemHomeAdapter(context, generalData.module)
-            itemHomeAdapter.setAdapter(generalData.module)
+            itemHomeAdapter?.setAdapter(generalData.module)
             binding.homeItemRv.adapter = itemHomeAdapter
 
             binding.allModules.setOnClickListener {
@@ -49,12 +52,22 @@ class GeneralAdapter(
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        holder.onBind(data[position])
+        holder.onBind(getItem(position))
     }
-
-    override fun getItemCount(): Int = data.size
 
     interface OnAllItemClick {
         fun onClick(course: Course)
+    }
+
+    class MyDiffUtil : DiffUtil.ItemCallback<GeneralData>() {
+
+        override fun areItemsTheSame(oldItem: GeneralData, newItem: GeneralData): Boolean {
+            return oldItem.course.id == newItem.course.id
+        }
+
+        override fun areContentsTheSame(oldItem: GeneralData, newItem: GeneralData): Boolean {
+            return oldItem.equals(newItem)
+        }
+
     }
 }
